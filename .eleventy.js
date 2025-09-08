1e1107026276aa70fff3317744c17c1d2680fda9
geekyteacher <87069314+nerraf-dev@users.noreply.github.com>
2025-09-08 15:25:55 +0200

    feat: Enhance Eleventy configuration with new filters and collections; update scripts and styles; refactor index and hero templates

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,26 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
   // Pass through static files, add shortcodes, etc.
   eleventyConfig.addPassthroughCopy("src/styles");
+  eleventyConfig.addPassthroughCopy("src/scripts");
+
+
+  // ***Filters:***
+  // date filter for Nunjucks
+  eleventyConfig.addFilter("date", (value, format = "yyyy-MM-dd") => {
+    if (!value) return "";
+    let dateObj = typeof value === "string" ? DateTime.fromISO(value, { zone: "utc" }) : DateTime.fromJSDate(value, { zone: "utc" });
+    return dateObj.toFormat(format);
+    // return DateTime.fromJSDate(value, { zone: "utc" }).toFormat(format);
+  });
+
+  // ***Collections:***
+  // Add a collection for food posts
+  eleventyConfig.addCollection("food", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/food/*.njk").reverse();
+  });
+
   return {
     dir: {
       input: "src",
